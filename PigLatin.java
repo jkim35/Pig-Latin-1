6505141299f2e61958abbb222bcf69b3acbfd2a6
duckonesie23 <155586579+jkim35@users.noreply.github.com>
2024-09-25 15:00:44 -0400

    ig
    ok

--- PigLatin.java
+++ PigLatin.java
@@ -1,11 +1,39 @@
-public class PigLatin{
+// Main.java			
+// Pig Latin Converter
+// This is the student starting file for Lab02bPigLatin.
 
-    public void isVowel(){
+import java.util.Scanner;
+
+public class PigLatin{
+    boolean vowel;
+	public static void main (String[] args)
+	{
+      		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a string: ");
+		String str = input.nextLine();
+        		 	
+		String piggie = PigLatin.convertWord(str);
+		System.out.println();
+		System.out.println("Converted word: " + piggie);
+	}
+    private boolean isVowel(String s){
+        if (s.equals("a")||s.equals("e")||s.equals("i")||s.equals("o")||s.equals("u")){
+            return true;
+        }else{
+        return false;
+        }
         
     }
-    public static void main (String[]args){
+    private static boolean startsVowel(String str){
+        if (str.isVowel() == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-    
-
+    private static String convertWord(String str) {
+        return null;
     }
 }
